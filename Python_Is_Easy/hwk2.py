e8317cf86573d6766e897fb62cec42e09edfa151
cod3-jr <lexicon_bda@hotmail.com>
2021-02-08 21:18:29 -0400

    homework 2 finished

---
+++ Python_Is_Easy/hwk2.py
@@ -0,0 +1,16 @@
+"""
+    Date:   2021-02-03
+    Dev:    Me
+    Purpose: list all of the attributes I think are needed
+    for storing details about my favorite song
+
+"""
+
+AlbumArt = "\Music Man\Best Album\winning"
+Artist = "Music Man"
+DurationSecs = 420 # length of song in seconds
+Genre  = "kaleidascope"
+Rating = 4.5
+Title = "winning"
+
+# print(AlbumArt,Artist,DurationSecs,Genre,Rating,Title, sep="\n", end="\n")
