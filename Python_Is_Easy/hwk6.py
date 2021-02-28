a2792bba554cab702c59eca87e267bf7ccb63acc
cod3-jr <lexicon_bda@hotmail.com>
2021-02-28 15:12:33 -0400

    hwk6.py complete. Meets Extra credit requirement

--- Python_Is_Easy/hwk6.py
+++ Python_Is_Easy/hwk6.py
@@ -9,32 +9,56 @@
     return: if width and height <= maximum, return True
         else return False
 """
+import math # only needed if printing row and / or column numbers
+import os # needed to determine terminal window size
 
 def drawPlayboard(rows, columns):
     dash = "-"
-    bar = " |"
-    # This if will add the appropriate number of rows to ensure the
-    # playboard has the requested number of rows
-    if rows % 2 == 0:
-        if rows > 3:
-            rows += 3
-        else:
-            rows += 1
+    bar = "|"
+    space = " "
+
+    # Handles printing small playboards
+    if columns > 2:
+        columns = columns * 2 -1
     else:
-        if rows > 3:
-            rows += 4
-        else:
-            rows += 2
-    # One bar creates 2 columns, so subtracting 1 from columns generates
-    # playboard with requested number of columns
-    columns -= 1
+        columns += 1
+    
+    if rows > 2:
+        rows = rows * 2 - 1
+    else:
+        rows += 1 
 
     for row in range(rows):
-        if row%2 == 0:
-            print(bar*columns)
-        else:
-            # adding one to finish row
-            print(dash*(columns*2+1))
+        # For printing row numbers
+        # if row % 2 == 0:
+        #     print(math.floor(row/2+1),end= " ")
+        # else:
+        #     print(" ", end= " ")
 
-
-drawPlayboard(6,4)
+        for column in range(columns):
+            if row % 2 == 0:
+                if column == (columns - 1):
+                    # For printing column number at end of row
+                    # print(math.floor(column/2+1))
+                    print(space)
+                elif column % 2 == 1:
+                    print(bar, end="")
+                else:
+                    # For printing column number
+                    # print(math.floor(column/2+1), end= "")
+                    print(space,end="")
+            elif column == columns - 1:
+                print(dash)
+            else:
+                print(dash,end= "")
+    
+    size = os.get_terminal_size()
+    if rows <= size.lines and columns <= size.columns:
+        return True
+    else:
+        return False
+# For confirming terminal window size
+# print(os.get_terminal_size())
+success = drawPlayboard(12,48)
+# For confirming function return value
+# print(success)
