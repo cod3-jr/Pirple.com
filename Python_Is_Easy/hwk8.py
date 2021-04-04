aa76623c5be589248467064422bb8019e213859b
cod3-jr <lexicon_bda@hotmail.com>
2021-04-04 17:25:08 -0300

    started homework8

---
+++ Python_Is_Easy/hwk8.py
@@ -0,0 +1,20 @@
+"""
+    2021-04-04
+    Requirements:
+        On start, prompt user for filename
+        If filename doesn't exist, prompt user to enter text
+            that will be written to the file.
+            After entering the text, it should save the file and exit
+        If the filename does exist, prompt user to:
+            a. read file
+                then: print file to terminal
+            b. overwrite file
+                then: delete file and create new file
+            c. append the file
+                then: add input to existing file
+    Bonus: 
+        implement 4th option: replace a single line
+        prompt for:
+            line to replace
+            text that should replace the line
+"""
