8daee47802cc378691a5a393c202be016148056b
cod3-jr <lexicon_bda@hotmail.com>
2021-08-17 23:46:55 -0300

    draws playboard

--- Python_Is_Easy/project1.py
+++ Python_Is_Easy/project1.py
@@ -6,15 +6,10 @@ import numpy as np
 # print(text)
 
 boardsize = [6,7]
+current = ""
+game_state = np.zeros(boardsize, dtype=int)
 player1 = 1
 player2 = 2
-game_state = np.zeros(boardsize, dtype=int)
-# game_state = np.array(
-#     [
-#         ["c0,r0","c0,r1","c0,r2"],
-#         ["c1,r0","c1,r1","c1,r2"],
-#         ["c2,r0","c2,r1","c2,r2"]
-#     ])
 
 def draw_Playboard(game_state):
     rows, columns = game_state.shape
@@ -25,8 +20,24 @@ def draw_Playboard(game_state):
     # reversing this range of how many rows draws the board top to bottom
     for rows in reversed(range(boardsize[1])):
         for columns in range(boardsize[0]):
-            print(game_state[columns][rows], end="")
+            cell = game_state[columns][rows]
+            if (cell != 0):
+                if (cell == player1):
+                    cprint("☉","red",attrs=["bold"],end="")
+                    # print(game_state[columns][rows], end="")
+                else:
+                    cprint("☉","grey",attrs=["bold"],end="")
+            else:
+                print(space,end="")
+            if (columns != boardsize[0]-1):
+                print(bar,end="")
         print()
+        print(dash*(boardsize[0]*2-1))
+
+    for i in range(boardsize[0]):
+        print(i,end=" ")
+    
+    print("\n")
 
 def nextMove(game_state, selected, player):
     # loop iterates through selected column array
@@ -42,9 +53,10 @@ def whoseTurn(player):
     else: 
         return player1
 
-# game_state[0,0] = player1
 nextMove(game_state,1,player1)
+
 nextMove(game_state,1,player2)
+nextMove(game_state,2,player1)
 draw_Playboard(game_state)
 
 # print(game_state)
